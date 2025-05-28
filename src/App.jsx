abf9b9bc4b45422f2aa7f580936b526cbae26641
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-05-27 21:19:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Text from "./components/text/text";
+function App() {
+  return (
+    <>
+      <Text />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/text/text.jsx
@@ -0,0 +1,13 @@
+import "./text.css";
+
+const Text = () => {
+  return (
+    <p className="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint unde ducimus
+      accusantium ipsam, magni repellat in natus odio ut fugiat obcaecati
+      exercitationem omnis dicta quo quis vero iusto necessitatibus praesentium!
+    </p>
+  );
+};
+
+export default Text;
